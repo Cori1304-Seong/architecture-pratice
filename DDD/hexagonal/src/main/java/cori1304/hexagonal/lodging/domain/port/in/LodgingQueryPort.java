31de0f804ee9653742146cb652023e6bcd65c030
Cori1304-Seong <andyjm7739@gmail.com>
2025-11-28 16:04:22 +0900

    remove copy files

--- DDD/hexagonal/src/main/java/cori1304/hexagonal/lodging/domain/port/in/LodgingQueryPort.java
+++
@@ -1,9 +0,0 @@
-package cori1304.hexagonal.lodging.domain.port.in;
-
-import cori1304.hexagonal.lodging.domain.model.Lodging;
-import java.util.List;
-
-public interface LodgingQueryPort {
-    Lodging getLodgingById(Long id);
-    List<Lodging> getAllLodgings();
-}
--- DDD/hexagonal/src/main/java/cori1304/hexagonal/lodging/domain/port/out/SaveLodgingPort.java
+++
@@ -1,7 +0,0 @@
-package cori1304.hexagonal.lodging.domain.port.out;
-
-import cori1304.hexagonal.lodging.domain.model.Lodging;
-
-public interface SaveLodgingPort {
-    Lodging saveLodging(Lodging lodging);
-}
